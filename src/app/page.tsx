e00f214eec08efeb0c89bcfe2caf5a6cf4048c6b
Kevin Pimenta <kevinpimenta@Kevins-MacBook-Air.local>
2026-02-22 14:33:29 +0530

    Enhance blue car position and 3D float animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
 
             {/* Right illustration */}
             <div className="hidden lg:flex items-center justify-center animate-fade-in-up" style={{ animationDelay: "0.2s" }}>
-              <div className="relative w-[120%] lg:w-[130%] aspect-[4/3] max-w-[700px] lg:-mr-20 animate-float" style={{ animationDelay: "0.5s" }}>
+              <div className="relative w-[120%] lg:w-[130%] aspect-[4/3] max-w-[700px] lg:-ml-16 animate-float" style={{ animationDelay: "0.5s" }}>
                 <Image
                   src="/bluecar.png"
                   alt="Premium Blue Car"
